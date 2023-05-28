d4da73a4d9b33cf133f9b8a0b1a6fa14b26662bf
howlagon <howl@foxes.lol>
2023-05-27 20:34:32 -0400

    version history :sunglas:

--- plugins/about.py
+++ plugins/about.py
@@ -1,8 +1,14 @@
-import platform
-
+from sys import version
 import lightbulb, hikari
-import ujson
+import ujson, os
 import env
+
+if os.path.exists(".git/COMMIT_EDITMSG"):
+    commit_message = open(".git/COMMIT_EDITMSG", "r", encoding="utf-8").read().strip("\n")
+    commit_hash = open(".git/FETCH_HEAD", "r", encoding="utf-8").read().split("\t")[0][:7]
+else:
+    commit_message = None
+    commit_hash = None
 
 plugin = lightbulb.Plugin("about")
 
@@ -12,7 +18,6 @@ plugin = lightbulb.Plugin("about")
 async def about(ctx: lightbulb.Context) -> None:
 
     authorids = ', '.join([f"<@{i}>" for i in ctx.bot.owner_ids])
-
     embed = hikari.Embed(
         title=f"About {env.NAME}",
         description=(
@@ -21,12 +26,14 @@ async def about(ctx: lightbulb.Context) -> None:
         url="https://github.com/howlagon/elwiwibot",
         colour=0xe9d5b5
     )
+
     embed.set_thumbnail(ctx.bot.get_me().avatar_url)
     embed.set_author(name="Bot Information")
     embed.set_footer(f"Requested by {ctx.member.display_name}", icon=ctx.member.avatar_url)
-    # embed.add_field("Bot version", f"`{commithash}`", inline=True)
-    # embed.add_field("Latest change", commitmsg, inline=True)
-    embed.add_field("Python version", f"`{platform.python_version()}`", inline=True)
+    if commit_hash or commit_message:
+        embed.add_field("Bot version", f"`{commit_hash}`", inline=True)
+        embed.add_field("Latest change", commit_message, inline=True)
+    embed.add_field("Python version", f"`{version}`", inline=True)
     embed.add_field("Hikari version", f"`{hikari.__version__}`", inline=True)
     embed.add_field("Lightbulb version", f"`{lightbulb.__version__}`", inline=True)
     
